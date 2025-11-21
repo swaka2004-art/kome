30947f7b45ae5d917474ff833a339025a41f0f12
swaka2004-art <s.waka2004@gmail.com>
2025-11-21 16:28:07 +0900

    （変更内容の説明）

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,20 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+document.addEventListener("DOMContentLoaded", () => {
+  const items = document.querySelectorAll(".category-item");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target); // 一度だけ発動
+      }
+    });
+  }, {
+    threshold: 0.2 // 20%見えたら反応
+  });
+
+  items.forEach(item => observer.observe(item));
+});
+
